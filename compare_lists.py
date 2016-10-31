41e96435e3d1fdd78773186cc9cf56db531d17f1
Fredrik Boulund <boulund@terra.math.chalmers.se>
2016-10-31 09:29:27 +0100

    New simple script for comparing two lists.

---
+++ compare_lists.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Compare metagenome taxonomic abundance rankings
+# using rank biased overlap.
+# Fredrik Boulund 2016
+
+from sys import argv, exit
+import argparse
+from measures.rankedlist import RBO, AverageOverlap
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("LIST1")
+    parser.add_argument("LIST2")
+    parser.add_argument("-p", 
+            default=0.98,
+            type=float,
+            help="p-value to use when comparing lists [%(default)s].")
+    parser.add_argument("-t", 
+            default=25,
+            type=int,
+            help="Number of top list values to display alongside comparison [%(default)s].")
+    
+    if len(argv) < 2:
+        parser.print_help()
+        exit(1)
+    
+    return parser.parse_args()
+
+
+def parse_list(fn):
+    with open(fn) as f:
+        f.readline() # Skip header lines
+        f.readline() # Skip header lines
+        for line in f:
+            if line.startswith("-"):
+                return
+            yield " ".join(line.split()[2:]).strip()
+
+
+def main(list1, list2, p=0.98, t=10):
+    for rank, items in enumerate(zip(list1[:t], list2[:t]), start=1):
+        if items[0] == items[1]:
+            print("{:>2} | {:<30} | =".format(rank, items[0]))
+        else:
+            print("{:>2} | {:<30} | {}".format(rank, *items))
+    print("Average Overlap score (only {} first entries):  {}".format(t, AverageOverlap.score(list1, list2, t)))
+    print("Rank Biased Overlap score (entire list):        {}".format(RBO.score(list1, list2, p)))
+
+
+if __name__ == "__main__":
+
+    options = parse_args()
+    list1 = list(parse_list(options.LIST1))
+    list2 = list(parse_list(options.LIST2))
+
+    main(list1, list2, options.p, options.t)
