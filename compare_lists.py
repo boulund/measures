62e5db0b9b55ce6245cf9ff8bf9bd2efa418c082
Fredrik Boulund <fredrik.boulund@ki.se>
2017-12-19 16:43:24 +0100

    Add --rbo, --ao to only output single float.

--- compare_lists.py
+++ compare_lists.py
@@ -18,6 +18,10 @@ def parse_args():
             help="p-value to use when comparing lists [%(default)s].")
     parser.add_argument("-t", default=25, type=int,
             help="Number of top list values to display alongside comparison [%(default)s].")
+    parser.add_argument("--rbo", action="store_true", default=False,
+            help="Only return the Rank-Based Overlap as a single float [%(default)s].")
+    parser.add_argument("--ao", action="store_true", default=False,
+            help="Only return the Average Overlap as a single float [%(default)s].")
     
     if len(argv) < 2:
         parser.print_help()
@@ -36,15 +40,21 @@ def parse_list(fn):
             yield " ".join(line.split()[2:]).strip()
 
 
-def main(list1, list2, p=0.98, t=10):
-    for rank, items in enumerate(zip(list1[:t], list2[:t]), start=1):
-        if items[0] == items[1]:
-            print("{:>4} | {:<60} | =".format(rank, items[0]))
-        else:
-            print("{:>4} | {:<60} | {}".format(rank, *items))
-    print("Average Overlap score (only {} first entries):  {}".format(t, AverageOverlap.score(list1, list2, t)))
-    print("Rank Biased Overlap score (entire list):        {}".format(RBO.score(list1, list2, p)))
-    print("-"*20)
+def main(list1, list2, p=0.98, t=10, rbo=False, ao=False):
+    if not any([rbo, ao]):
+        for rank, items in enumerate(zip(list1[:t], list2[:t]), start=1):
+            if items[0] == items[1]:
+                print("{:>4} | {:<60} | =".format(rank, items[0]))
+            else:
+                print("{:>4} | {:<60} | {}".format(rank, *items))
+        print("Average Overlap score (only {} first entries):  {}".format(t, AverageOverlap.score(list1, list2, t)))
+        print("Rank Biased Overlap score (entire list):        {}".format(RBO.score(list1, list2, p)))
+        print("-"*20)
+    else:
+        if rbo:
+            print(RBO.score(list1, list2, p))
+        elif ao:
+            print(AverageOverlap.score(list1, list2, t))
 
 
 if __name__ == "__main__":
@@ -53,6 +63,7 @@ if __name__ == "__main__":
     list1 = list(parse_list(options.LIST1))
     list2 = list(parse_list(options.LIST2))
 
-    print("Comparing '{}' with '{}'".format(options.LIST1, options.LIST2))
-    print("-"*20)
-    main(list1, list2, options.p, options.t)
+    if not any([options.rbo, options.ao]):
+        print("Comparing '{}' with '{}'".format(options.LIST1, options.LIST2))
+        print("-"*20)
+    main(list1, list2, options.p, options.t, options.rbo, options.ao)
